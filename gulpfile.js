7769e85a54a8a078685b5645fcc0714ec752295c
sokolov pavel <sokolovpavel7@gmail.com>
2023-10-08 19:18:41 +0300

    gulp task default

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
+const server = require('gulp-server-livereload');
+const clean = require('gulp-clean');
+const fs = require('fs');
 
-gulp.task('includeFiles', () => {
+gulp.task('html', () => {
     return gulp
         .src('./src/*.html')
         .pipe(
@@ -15,5 +18,45 @@ gulp.task('includeFiles', () => {
 });
 
 gulp.task('sass', () => {
-    return gulp.src('./src/scss/*.scss').pipe(sass()).pipe(gulp.dest('./dist/css/'));
+    return gulp
+        .src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/css/'));
 });
+
+gulp.task('images', () => {
+    return gulp.src('./src/img/**/*').pipe(gulp.dest('./dist/img/'));
+});
+
+gulp.task('server', () => {
+    return gulp.src('./dist/').pipe(
+        server({
+            livereload: true,
+            open: true,
+        })
+    );
+});
+
+gulp.task('clean', (done) => {
+    if (fs.existsSync('./dist/')) {
+        return gulp
+            .src('./dist/', { read: false })
+            .pipe(clean({ force: true }));
+    }
+    done();
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('./src/**/*.html', gulp.parallel('html'));
+    gulp.watch('./src/img/**/*', gulp.parallel('images'));
+});
+
+gulp.task(
+    'default',
+    gulp.series(
+        'clean',
+        gulp.parallel('html', 'sass', 'images'),
+        gulp.parallel('server', 'watch')
+    )
+);
